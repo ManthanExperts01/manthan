cd257ef83a6641e75b9f11442ae2d8d757a6d21e
ManthanExperts01 <manthanexperts123@gmail.com>
2025-04-10 18:35:08 +0530

    manthan to manthanam

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -216,7 +216,7 @@ const Footer = () => {
 
           <div className="bg-black/5 p-6 text-center">
             <span>© 2024 Copyright:</span>
-            <span> Manthan Experts</span>
+            <span> Manthan Experts (known as Manthanam Experts Pvt Ltd.)</span>
           </div>
         </div>
       </footer>
